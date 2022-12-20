6db06e82e7dc78f37f8b56323afa677f17b29516
Ma7moud Kassem <Ma7moud Kassem@DESKTOP-9IVLO3P>
2022-12-20 23:40:00 +0200

    841. Keys and Rooms

---
+++ 841. Keys and Rooms/Program.cs
@@ -0,0 +1,18 @@
+﻿
+HashSet<int> enteredRoom = new();
+bool CanVisitAllRooms(IList<IList<int>> rooms)
+{
+    EnterRoom(0, rooms);
+
+    return enteredRoom.Count() == rooms.Count();
+}
+void EnterRoom(int roomKey, IList<IList<int>> rooms)
+{
+    enteredRoom.Add(roomKey);
+    IList<int> keysInRoom = rooms[roomKey];
+    foreach (int key in keysInRoom)
+    {
+        if (!enteredRoom.Contains(key))
+            EnterRoom(key, rooms);
+    }
+}
