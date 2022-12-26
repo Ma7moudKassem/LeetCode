4a8d9d7447dbd477ea9617255422e91bb42918a7
Ma7moud Kassem <Ma7moud Kassem@DESKTOP-9IVLO3P>
2022-12-26 15:26:27 +0200

    88. Merge Sorted Array

--- 88. Merge Sorted Array/88. Merge Sorted Array.cpp
+++ 88. Merge Sorted Array/88. Merge Sorted Array.cpp
@@ -7,7 +7,8 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
 	int i = m - 1;
 	int j = n - 1;
 
-	while (i >= 0 && j >= 0) {
+	while (i >= 0 && j >= 0) 
+	{
 		if (nums2[j] > nums1[i]) 
 			nums1[max--] = nums2[j--];
 		else 
