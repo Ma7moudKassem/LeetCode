d70b5471b1e568b4481a4198f37b488ccc93571f
Ma7moud Kassem <Ma7moud Kassem@DESKTOP-9IVLO3P>
2022-12-28 09:47:15 +0200

    350. Intersection of Two Arrays II

--- 350. Intersection of Two Arrays II/350. Intersection of Two Arrays II.cpp
+++ 350. Intersection of Two Arrays II/350. Intersection of Two Arrays II.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-	vector<int> intersectedArr;
-	int i = 0;
-	while (i < nums1.size())
-	{
-		for (int j = 0; j < nums2.size(); j++)
-		{
-			if (nums1[i] == nums2[j])
-			{
-				intersectedArr.push_back(nums1[i]);
-				if ((nums2.size() == 1) || (i == nums2.size()-1))
-					return intersectedArr;
-
-				break;
-			}
-		}
-		i++;
-	}
-
-	return intersectedArr;
+    if (nums1.size() == 0 || nums2.size() == 0)
+        return vector<int>();
+    unordered_map<long, int> map;
+    vector<int> res;
+    for (int i = 0; i < nums1.size(); i++) {
+        map[nums1[i]]++;
+    }
+    for (int i = 0; i < nums2.size(); i++) {
+        if (map[nums2[i]] > 0) {
+            res.push_back(nums2[i]);
+            map[nums2[i]]--;
+        }
+    }
+    return res;
 }
 
 int main()
