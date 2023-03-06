a43926c26917fecbfd277d5a5d90798db4256733
Mahmoud Kassem <modykassem123@gmail.com>
2023-03-06 13:31:02 +0200

    14. Longest Common Prefix

---
+++ 14. Longest Common Prefix/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] arr = { "ab", "a" };
+
+Console.WriteLine(LongestCommonPrefix(arr));
+
+
+
+string LongestCommonPrefix(string[] strs)
+{
+    if (strs is null || !strs.Any() || (strs.Count(e => e == "") > 0)) return "";
+
+    int i = 0;
+
+    string result = string.Empty;
+    while (i < strs[0].Length)
+    {
+        string provider = strs[0][i].ToString();
+        bool isMatch = false;
+        foreach (string str in strs)
+        {
+            if (str.Length == i) return result;
+            if (str[i].ToString() == provider)
+                isMatch = true;
+            else return result;
+        }
+
+        result = result + provider;
+        i++;
+    }
+
+    return result;
+}
