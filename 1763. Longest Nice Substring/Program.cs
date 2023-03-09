7d4e90a1725df8ef82781635f11bedcbcbd7d0d5
Mahmoud Kassem <modykassem123@gmail.com>
2023-03-09 16:33:26 +0200

    1763. Longest Nice Substring

---
+++ 1763. Longest Nice Substring/Program.cs
@@ -0,0 +1,26 @@
+﻿var m = LongestNiceSubstring("cChH");
+
+
+Console.WriteLine();
+string LongestNiceSubstring(string s)
+{
+    return DFS(s);
+}
+
+string DFS(string s)
+{
+    HashSet<char> hashSet = new HashSet<char>(s);
+    if (s.Length < 2)
+        return "";
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (!hashSet.Contains(char.ToLower(s[i])) || !hashSet.Contains(char.ToUpper(s[i])))
+        {
+            string str1 = DFS(s.Substring(0, i));
+            string str2 = DFS(s.Substring(i + 1, s.Length - i - 1));
+            return str2.Length > str1.Length ? str2 : str1;
+        }
+    }
+    return s;
+}
