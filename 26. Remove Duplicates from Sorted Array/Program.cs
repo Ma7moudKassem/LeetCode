e555ac58dda6ccbfe6572a8e26cbdc5df5c0755e
Mahmoud Kassem <modykassem123@gmail.com>
2023-03-16 18:24:34 +0200

    26. Remove Duplicates from Sorted Array

---
+++ 26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+Console.WriteLine(RemoveDuplicates(nums));
+
+int RemoveDuplicates(int[] nums)
+{
+    if (nums == null || nums.Length == 0)
+        return 0;
+    else if (nums.Length == 1)
+        return 1;
+
+    int index1 = 0,
+        index2 = 1;
+
+    while (index2 <= nums.Length - 1)
+        if (nums[index1] == nums[index2])
+            index2++;
+        else
+            nums[++index1] = nums[index2++];
+
+    return ++index1;
+}
