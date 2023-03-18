3ba12b6a50d36af6e714e4586ac7180c75dc6e1c
Mahmoud Kassem <modykassem123@gmail.com>
2023-03-18 09:33:00 +0200

    58. Length of Last Word

---
+++ 58. Length of Last Word/Program.cs
@@ -0,0 +1,22 @@
+﻿//Console.WriteLine(LengthOfLastWord("Hello World")); 
+//Console.WriteLine(LengthOfLastWord("   fly me   to   the moon  ")); 
+//Console.WriteLine(LengthOfLastWord("luffy is still joyboy")); 
+//Console.WriteLine(LengthOfLastWord("")); 
+Console.WriteLine(LengthOfLastWord("a")); 
+
+int LengthOfLastWord(string s)
+{
+
+    List<string> strs = s.Split(' ').ToList();
+
+    int i = strs.Count - 1;
+    while (i >= 0)
+    {
+        if (strs[i] != "")
+            return strs[i].Length;
+
+        i--;
+    }
+
+    return 0;
+}
