181c1810c7b6f3f141ce1fbfb7d797b21c5eaa1f
Mahmoud Kassem <modykassem123@gmail.com>
2023-05-07 07:41:56 +0300

    Runtime 146 ms Memory 42.8 MB

---
+++ 27. Remove Element/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arr = { 3, 2, 2, 3 };
+
+RemoveElement(arr, 3);
+int RemoveElement(int[] nums, int val)
+{
+    int j = 0;
+    for (int i = 0; i < nums.Length; i++)
+        if (nums[i] != val)
+            nums[j++] = nums[i];
+
+    return j;
+}
