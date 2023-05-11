23b6dd63a1672a8a7c1d4d229b11f3a6954e651c
Mahmoud Kassem <modykassem123@gmail.com>
2023-05-11 07:55:51 +0300

    Runtime 145 ms  Memory 42.9 MB

---
+++ 66. Plus One/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] arr = new int[] { 1 };
+
+
+PlusOne(arr);
+int[] PlusOne(int[] digits)
+{
+
+    int length = digits.Length;
+    for (int i = digits.Length - 1; i >= 0; i--)
+    {
+        if (digits[i] < 9)
+        {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+
+    int[] newArr = new int[length + 1];
+    newArr[0] = 1;
+
+    return newArr;
+}
