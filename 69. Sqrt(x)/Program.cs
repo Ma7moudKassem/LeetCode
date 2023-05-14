238c2a9517b97810602d415ccf5ffe5996174157
Mahmoud Kassem <modykassem123@gmail.com>
2023-05-14 09:59:14 +0300

    Runtime 20 ms  Memory 26.8 MB

---
+++ 69. Sqrt(x)/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine(MySqrt(2147483647));
+int MySqrt(int x)
+{
+    if (x == 0)
+        return 0;
+
+    int left = 1;
+    int right = x;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        int sqrt = x / mid;
+
+        if (sqrt == mid)
+            return mid;
+        else if (sqrt < mid)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+
+    return right;
+}
