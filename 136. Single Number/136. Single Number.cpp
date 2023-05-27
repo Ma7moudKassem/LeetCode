e0355f63f668c68bfb22ea7d29f3f6b494096189
Mahmoud Kassem <modykassem123@gmail.com>
2023-05-27 09:14:29 +0300

    Runtime 34 ms Memory 16.9 MB

---
+++ 136. Single Number/136. Single Number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <Vector>
+#include <algorithm>
+using namespace std;
+
+int singleNumber(vector<int>& nums) 
+{
+	sort(nums.begin(), nums.end());
+
+	for (int i = 1; i < nums.size(); i += 2)
+		if (nums[i] != nums[i-1])
+			return nums[i-1];
+	return nums[nums.size() - 1];
+}
+
+// [4,1,2,1,2] --
+
+int main()
+{
+	vector<int> arr = { 4,1,2,1,2 };
+
+	cout << singleNumber(arr);
+}
