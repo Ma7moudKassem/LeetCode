96fdf66bab3225f6fc548c9c07d35e720f35968b
Mahmoud Kassem <modykassem123@gmail.com>
2023-05-27 07:37:29 +0300

    Runtime 3 ms Memory 8 MB

---
+++ 125. Valid Palindrome/125. Valid Palindrome.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+bool isPalindrome(string s) {
+
+	string sTolower;
+	for (auto c : s)
+	{
+		if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c >= '0' && c <= '9')
+			sTolower.push_back(tolower(c));
+	}
+
+	string revString = sTolower;
+	reverse(revString.begin(), revString.end());
+	return sTolower == revString;
+}
+
+int main()
+{
+	cout << isPalindrome("0P");
+}
