e6cf076a0b6770bd185bfa0aeea8c7f793ce95e8
Mahmoud Kassem <modykassem123@gmail.com>
2023-06-01 10:11:42 +0300

    Runtime 0 ms Memory 6 MB

---
+++ 190. Reverse Bits/190. Reverse Bits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+uint32_t reverseBits(uint32_t n) {
+
+	bitset<32> bi(n);
+	string str = bi.to_string();
+
+	reverse(str.begin(), str.end());
+
+	bitset<32> b2(str);
+	return b2.to_ullong();
+}
+
+int main()
+{
+}
