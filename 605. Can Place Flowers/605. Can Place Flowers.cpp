c8b58b0a17de73ee0f5b699eabb6c17533f9969f
Mahmoud Kassem <modykassem123@gmail.com>
2023-06-04 13:11:19 +0300

    Runtime 19 ms Memory 20.4 MB

---
+++ 605. Can Place Flowers/605. Can Place Flowers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+
+	flowerbed.insert(flowerbed.begin(), 0);
+	flowerbed.push_back(0);
+
+	for (int i = 1; i < flowerbed.size() - 1; i++)
+		if (flowerbed[i] + flowerbed[i + 1] + flowerbed[i - 1] == 0)
+		{
+			n--;
+			i++;
+		}
+
+	if (n <= 0)
+		return true;
+
+	return false;
+}
+int main()
+{
+	vector<int> flowerbed = { 0,0,1,0,1 };
+	cout << canPlaceFlowers(flowerbed, 1);
+}
