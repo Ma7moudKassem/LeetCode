3cdb463b66eca21468f472ed05370b10542576c4
Mahmoud Kassem <modykassem123@gmail.com>
2023-07-14 15:42:28 +0300

    Runtime 124ms  Memory 53.15mb

---
+++ 131. Palindrome Partitioning/131. Palindrome Partitioning.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isPalndrome(string s, int left, int right)
+{
+	while (left < right)
+	{
+		if (s[left++] != s[right--])
+			return false;
+	}
+
+	return true;
+}
+void backtarck(vector<vector<string>>& ans, vector<string>& res, string& s, int index)
+{
+	if (index == s.size())
+	{
+		res.push_back(s);
+		return;
+	}
+
+	for (int i = index; i < s.size(); i++)
+	{
+		if (isPalndrome(s, index, i))
+		{
+			res.push_back(s.substr(index, i - index + 1));
+			backtarck(ans, res, s, i + 1);
+			res.pop_back();
+		}
+	}
+}
+vector<vector<string>> partition(string s) {
+	vector<vector<string>> ans;
+	vector<string> res;
+
+	backtarck(ans, res, s, 0);
+
+	return ans;
+}
+
+int main()
+{
+}
