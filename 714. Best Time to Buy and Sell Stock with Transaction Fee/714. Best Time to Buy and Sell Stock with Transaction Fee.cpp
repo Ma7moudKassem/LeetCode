f667bbe1074d3fedbe0d326758387c6398dba968
Mahmoud Kassem <modykassem123@gmail.com>
2023-07-24 01:23:06 +0300

    Runtime Beats 85.2% Memory Beats 80.5%

--- 714. Best Time to Buy and Sell Stock with Transaction Fee/714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee/714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -3,36 +3,22 @@
 #include <unordered_map>
 using namespace std;
 
-int maxProfit(vector<int>& prices) {
-	int firstBuy = INT_MAX,
-		firstProfit = INT_MIN,
-		secondBuy = INT_MAX,
-		secondProfit = INT_MIN;
+int maxProfit(vector<int>& prices, int fee) {
+
+	int minPrice = INT_MAX;
+	int maxProfit = 0;
 
 	for (int price : prices)
 	{
-		firstBuy = min(firstBuy, price);
-		firstProfit = max(firstProfit, price - firstBuy);
-		secondBuy = min(secondBuy, price - firstProfit);
-		secondProfit = max(secondProfit, price - secondBuy);
+		minPrice = min(minPrice, price - maxProfit);
+		maxProfit = max(maxProfit, price - minPrice - fee);
 	}
 
-	return secondProfit;
+	return maxProfit;
 }
-//int maxProfit(vector<int>& prices, int fee) {
-//	int maxProfit = 0, minPrice = INT_MAX;
-//
-//	for (int price : prices)
-//	{
-//		minPrice = min(minPrice, maxProfit - price);
-//		maxProfit = max(maxProfit, (price - minPrice) - fee);
-//	}
-//
-//	return maxProfit;
-//}
 
 int main()
 {
-	vector<int> arr = { 3,3,5,0,0,3,1,4 };
-	cout << maxProfit(arr);
+	vector<int> arr = { 1,3,2,8,4,9 };
+	cout << maxProfit(arr, 2);
 }
