8a6a91c4de621b4541e443037591a3d1e5875d2e
Mahmoud Kassem <modykassem123@gmail.com>
2023-07-26 12:56:27 +0300

    Runtime Beats 75.89% Memory Beats 61.39%

--- 1282. Group the People Given the Group Size They Belong To/1282. Group the People Given the Group Size They Belong To.cpp
+++ 1282. Group the People Given the Group Size They Belong To/1282. Group the People Given the Group Size They Belong To.cpp
@@ -48,29 +48,50 @@ using namespace std;
 #pragma endregion
 
 //,3,3,3,3,1,3  
-vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+//vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+//
+//	int n = groupSizes.size();
+//	unordered_map<int, vector<int>> map;
+//	vector<vector<int>> ans;
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		map[groupSizes[i]].push_back(i);
+//
+//		if (map[groupSizes[i]].size() == groupSizes[i])
+//		{
+//			ans.push_back(map[groupSizes[i]]);
+//			map[groupSizes[i]] = {};
+//		}
+//	}
+//
+//	return ans;
+//}
 
-	int n = groupSizes.size();
-	unordered_map<int, vector<int>> map;
-	vector<vector<int>> ans;
 
+int lengthOfLongestSubstring(string s) {
+	int ans = 0, n = s.size(), count = 0;
+
+	unordered_map<char, int> map;
+
+	int j = 0;
 	for (int i = 0; i < n; i++)
 	{
-		map[groupSizes[i]].push_back(i);
+		if (map[s[i]] > 0)
+			j = max(j, map[s[i]]);
 
-		if (map[groupSizes[i]].size() == groupSizes[i])
-		{
-			ans.push_back(map[groupSizes[i]]);
-			map[groupSizes[i]] = {};
-		}
+		count = max(count, i - j + 1);
+
+		map[s[i]] = i + 1;
 	}
 
-	return ans;
+	return count;
 }
 
 int main()
 {
 	vector<int> arr = { 3,3,3,3,3,1,3 };
 
-	groupThePeople(arr);
+	//aab
+	cout << lengthOfLongestSubstring("abcabcbb");
 }
