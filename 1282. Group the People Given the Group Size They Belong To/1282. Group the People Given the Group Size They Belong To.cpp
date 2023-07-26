67ddb4a06f19103a3fcb4699905c68d40c299b5b
Mahmoud Kassem <modykassem123@gmail.com>
2023-07-26 10:45:49 +0300

    Runtime Beats 81.85% Memory Beats 94.95%

--- 1282. Group the People Given the Group Size They Belong To/1282. Group the People Given the Group Size They Belong To.cpp
+++ 1282. Group the People Given the Group Size They Belong To/1282. Group the People Given the Group Size They Belong To.cpp
@@ -47,21 +47,25 @@ using namespace std;
 //}
 #pragma endregion
 
+//,3,3,3,3,1,3  
 vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
 
-	vector<vector<int>> res;
-	unordered_map<int, vector<int>> groups;
+	int n = groupSizes.size();
+	unordered_map<int, vector<int>> map;
+	vector<vector<int>> ans;
 
-	for (int i = 0; i < groupSizes.size(); i++) {
-		auto& group = groups[groupSizes[i]];
-		group.push_back(i);
-		if (group.size() == groupSizes[i]) {
-			res.push_back(group);
-			group = {};
+	for (int i = 0; i < n; i++)
+	{
+		map[groupSizes[i]].push_back(i);
+
+		if (map[groupSizes[i]].size() == groupSizes[i])
+		{
+			ans.push_back(map[groupSizes[i]]);
+			map[groupSizes[i]] = {};
 		}
 	}
 
-	return res;
+	return ans;
 }
 
 int main()
