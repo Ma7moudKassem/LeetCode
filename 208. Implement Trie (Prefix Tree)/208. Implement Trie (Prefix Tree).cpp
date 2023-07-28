8420040ac34c09ebd4a09d61ae17f27ad63855a5
Mahmoud Kassem <modykassem123@gmail.com>
2023-07-28 15:06:17 +0300

    Runtime Beats 98.98% Memory Beats 83.21%

--- 208. Implement Trie (Prefix Tree)/208. Implement Trie (Prefix Tree).cpp
+++ 208. Implement Trie (Prefix Tree)/208. Implement Trie (Prefix Tree).cpp
@@ -2,57 +2,87 @@
 #include <stack>
 using namespace std;
 
+class TrieNode
+{
+public:
+	TrieNode* children[26];
+	bool isEnd;
+};
+
 class Trie {
 public:
-	stack<string> data;
 	Trie() {
 
 	}
 
+	TrieNode* data = new TrieNode();
+
+private:
+	TrieNode* newChild()
+	{
+		TrieNode* node = new TrieNode();
+
+		for (int i = 0; i < 26; i++)
+			node->children[i] = NULL;
+
+		node->isEnd = false;
+
+		return node;
+	}
+
+public:
 	void insert(string word) {
-		data.push(word);
+		TrieNode* node = data;
+
+		for (int i = 0; i < word.size(); i++)
+		{
+			int idx = word[i] - 'a';
+
+			if (!node->children[idx])
+				node->children[idx] = newChild();
+
+			node = node->children[idx];
+		}
+
+		node->isEnd = true;
 	}
 
 	bool search(string word) {
-		stack<string> temp = data;
+		TrieNode* node = data;
 
-		if (temp.size())
+		for (int i = 0; i < word.size(); i++)
 		{
-			while (temp.size())
-			{
-				if (temp.top() == word)
-					return true;
+			int idx = word[i] - 'a';
 
-				temp.pop();
-			}
+			if (!node->children[idx])
+				return false;
+
+			node = node->children[idx];
 		}
 
-		return false;
+		return node->isEnd;
 	}
 
 	bool startsWith(string prefix) {
-		if (data.size())
-		{
-			string first = data.top();
+		TrieNode* node = data;
 
-			if (prefix.size() > first.size())
+		for (int i = 0; i < prefix.size(); i++)
+		{
+			int idx = prefix[i] - 'a';
+
+			if (!node->children[idx])
 				return false;
 
-			for (int i = 0; i < first.size(); i++)
-				if (first[i] != prefix[i])
-					return false;
-
-			return true;
+			node = node->children[idx];
 		}
 
-		return false;
+		return true;
 	}
 };
 
-
 int main()
 {
-	Trie* obj = new Trie();
+	/*Trie* obj = new Trie();
 	obj->insert("ab");
 	bool param_5 = obj->search("abc");
 	bool param_6 = obj->search("ab");
@@ -63,5 +93,6 @@ int main()
 	bool param_2 = obj->startsWith("abc");
 	obj->insert("abc");
 	bool param_8 = obj->search("abc");
-	bool param_4 = obj->startsWith("ac");
+	bool param_4 = obj->startsWith("ac");*/
+
 }
