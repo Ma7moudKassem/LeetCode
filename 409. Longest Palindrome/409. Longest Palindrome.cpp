9de670adf33d2cb56057cee4a884e7af9a2c9140
Mahmoud Kassem <modykassem123@gmail.com>
2023-09-13 21:27:20 +0300

    Runtime Beats 100.00% Memory Beats 6.65%

---
+++ 409. Longest Palindrome/409. Longest Palindrome.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int longestPalindrome(string s) {
+	vector<char, int> arr(128);
+
+	for (auto c : s)
+		arr[c]++;
+
+	int ans = 0;
+	for (auto c : arr)
+	{
+		ans += c / 2 * 2;
+
+		if (ans % 2 == 0 && c % 2 == 1)
+			ans++;
+	}
+
+	return ans;
+}
+
+int main()
+{
+	cout << longestPalindrome("abccccdd");
+}
