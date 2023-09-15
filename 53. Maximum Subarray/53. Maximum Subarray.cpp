d77c9aab560656a1fc281416fcd26521880a00c1
Mahmoud Kassem <modykassem123@gmail.com>
2023-09-15 21:03:26 +0300

    Runtime 86 ms  Memory 70.7 MB

--- 53. Maximum Subarray/53. Maximum Subarray.cpp
+++ 53. Maximum Subarray/53. Maximum Subarray.cpp
@@ -2,66 +2,17 @@
 #include <vector>
 using namespace std;
 
-//vector<int> DisSortedArray(vector<int>& nums)
-//{
-//	int i = 0;
-//	int count = 0;
-//	while (true)
-//	{
-//		if (i >= nums.size() || i == nums.size() - 1) { i = 0; count++; }
-//		if (nums[i] < nums[i + 1])
-//		{
-//			int temp = nums[i];
-//			nums[i] = nums[i + 1];
-//			nums[i + 1] = temp;
-//		}
-//		i++;
-//
-//		if (count == nums.size()) { return nums; }
-//	}
-//}
+int maxSubArray(vector<int>& nums) {
+	vector<int> dp(nums);
 
-/* ---- Using Force O(N2)
-int maxSubArray(vector<int>& nums)
-{
-	int max = nums[0];
-	int sum = 0;
-	int count = 0;
-	for (int i = 0; i < nums.size(); i++)
-	{
-		for (int j = i + 1; j < nums.size(); j++)
-		{
-			sum = sum + nums[j];
-			sum = sum + nums[i];
-			if (sum > max) { max = sum; sum = sum - nums[i]; }
-			else { sum = sum - nums[i]; };
-		}
-		sum = 0;
-	}
-	for (int i = 0; i < nums.size(); i++)
-	{
-		if (nums[i] > max) max = nums[i];
-	}
-	return max;
-}
-*/
+	for (int i = 1; i < nums.size(); i++)
+		dp[i] = max(nums[i], nums[i] + dp[i - 1]);
 
-int maxSubArray(vector<int>& nums)
-{
-	int leader = nums[0];
-	int curr_sum = nums[0];
-
-	for (int i = 1; i < nums.size(); i++) 
-	{
-		curr_sum = max(nums[i], nums[i] + curr_sum);
-		leader = max(leader, curr_sum);
-	}
-
-	return leader;
+	return *max_element(dp.begin(), dp.end());
 }
 
 int main()
 {
 	vector<int> nums = { -2,1,-3,4,-1,2,1,-5,4 };
-	int m = maxSubArray(nums);
+	cout << maxSubArray(nums);
 }
