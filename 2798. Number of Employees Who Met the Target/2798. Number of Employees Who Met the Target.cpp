84ff1b3d2d161c5101a20942fe3b12a1998f8511
Mahmoud Kassem <modykassem123@gmail.com>
2023-09-23 15:24:03 +0300

    Runtime Beats 64.14% Memory 20.75MB

---
+++ 2798. Number of Employees Who Met the Target/2798. Number of Employees Who Met the Target.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+	int ans = 0;
+
+	for (int hour : hours)
+		if (hour >= target)
+			ans++;
+
+	return ans;
+}
+
+int main()
+{
+}
