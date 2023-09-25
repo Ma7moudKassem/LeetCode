6c79a9f2b87168d88f7a03909bfd473e92990c8e
Mahmoud Kassem <modykassem123@gmail.com>
2023-09-25 22:10:45 +0300

    Runtime Beats 90.97% Memory Beats 45.74%

---
+++ 2149. Rearrange Array Elements by Sign/2149. Rearrange Array Elements by Sign.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> rearrangeArray(vector<int>& nums) {
+
+	vector<int> ans(nums.size(), 0);
+	int i = 0, j = 1;
+	for (auto num : nums)
+	{
+		if (num > 0)
+		{
+			ans[i] = num;
+			i += 2;
+		}
+		else
+		{
+			ans[j] = num;
+			j += 2;
+		}
+	}
+
+	return ans;
+}
+
+int main()
+{
+}
