24656ce5c133b0dbd6fa7d24c586d3904f2d6297
Mahmoud Kassem <modykassem123@gmail.com>
2024-05-06 22:32:30 +0300

    Runtime 45 ms Memory 39.64 MB

---
+++ C#/20. Valid Parentheses/Program.cs
@@ -0,0 +1,29 @@
+﻿string input = "()[]{}";
+
+Console.WriteLine(IsValid(input));
+bool IsValid(string s)
+{
+    var stack = new Stack<char>();
+
+    foreach (char c in s)
+    {
+        if (c == '(')
+        {
+            stack.Push(')');
+            continue;
+        }
+        if (c == '{')
+        {
+            stack.Push('}');
+            continue;
+        }
+        if (c == '[')
+        {
+            stack.Push(']');
+            continue;
+        }
+        if (stack.Count == 0 || c != stack.Pop()) return false;
+    }
+
+    return stack.Count == 0;
+}
